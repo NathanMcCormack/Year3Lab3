039a523397698d20638689576a63141b20aa60d9
Nathan McCormack <G00414780@atu.ie>
2024-10-15 10:25:34 +0100

    Added Array list and GET request method

--- src/main/java/ie/atu/lab3/PersonController.java
+++ src/main/java/ie/atu/lab3/PersonController.java
@@ -1,14 +1,26 @@
 package ie.atu.lab3;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
-    @PostMapping("/person/createPerson")
+
+    List<Person> personList = new ArrayList<>();
+
+    @GetMapping
+    public List<Person> getAllPerson(){
+        return personList;
+    }
+
+    @PostMapping("/createPerson")
     public String addPerson(@RequestBody @Valid Person personRequest){
+
+        personList.add(personRequest);
         return "Details added";
     }
 }
